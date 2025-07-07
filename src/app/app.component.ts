c05322650bd64943913129291e9db411bbc4152c
Kevin Hidalgo <kj.hidalgo@itq.com.ec>
2025-07-07 15:23:07 -0500

    Actualizacion del titulo en app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'formulario';
+  title = 'Login Angular con Material';
 }
